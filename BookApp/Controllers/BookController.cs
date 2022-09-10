4f1db1481affd0d44f6b0c4a11ced8a722fa7f11
Dharik Mohamed <dharikmohamed13@gmail.com>
2022-09-10 19:06:31 +0530

    Project added

---
+++ BookApp/Controllers/BookController.cs
@@ -0,0 +1,171 @@
+﻿using BookApp.Models;
+using DataModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Net.Http.Headers;
+using System.Windows.Forms;
+
+namespace BookApp.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public BookController(ApplicationContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpPost]
+        public BookListResult SaveBookDetails([FromBody] BookListRequest instance)
+        {
+            BookListResult res = new BookListResult();
+            try
+            {
+                if (instance.BookList != null && instance.BookList.Count() > 0)
+                {
+                    using (var context = new KnilaTestEntities())
+                    {
+                        var clientIdParameter = new SqlParameter("@ParamList", instance.BookList);
+                        var clientIdParameter1 = new SqlParameter("@TransTypeID", 1);
+                        var clientIdParameter2 = new SqlParameter("@Res", "");
+                        clientIdParameter.Direction = ParameterDirection.Output;
+
+                        var result = context.Database
+                            .SqlQuery<string>("splocal_Book_IUD @ParamList,@TransTypeID, @Res OUT", clientIdParameter, clientIdParameter1, clientIdParameter2);
+                        string resultString = result.FirstOrDefault();
+
+                        res.ErrorMessage = resultString;
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                res.HasError = true;
+                res.ErrorMessage = e.Message;
+            }
+
+
+            return res;
+        }
+
+        [HttpGet]
+        public BookPrice GetPriceofAllBooks()
+        {
+            BookPrice res = new BookPrice();
+            using (var context = new KnilaTestEntities())
+            {
+                var query = (from PU in context.tblBook
+                                select PU.Price).Sum();
+                res.SumofPrice = Convert.ToDecimal(query);
+                
+            }
+
+            return res;
+        }
+        [HttpGet]
+        public BookListPLFT GetSortedListByPLFT()
+        {
+            BookListPLFT res = new BookListPLFT();
+            try
+            {
+                using (var context = new KnilaTestEntities())
+                {
+                    var query = (from PU in context.tblBook
+                                 orderby PU.Publisher,PU.AuthorLastName, PU.AuthorFirstName,PU.Title 
+                                 select PU).ToList();
+                    if(query.Count() > 0)
+                    {
+
+                        for (int i = 0; i < query.Count; i++)
+                        {
+                            res.bookList.Add(new BookDatatype
+                            {
+                                AuthorLastName = query[i].AuthorLastName,
+                                AuthorFirstName = query[i].AuthorFirstName,
+                                Publisher = query[i].Publisher,
+                                Title = query[i].Title,
+                                Price = Convert.ToDecimal( query[i].Price),
+                                TitleofContainer = query[i].TitleofContainer,
+                                PublicationDate =Convert.ToDateTime( query[i].PublicationDate),
+                                PageNo = Convert.ToInt32(query[i].PageNo),
+                                JournalTitle = query[i].JournalTitle,
+                                Issueno = query[i].Issueno,
+                                VolumeNo = query[i].VolumeNo,
+                                PageRange = query[i].PageRange,
+                                PageURL = query[i].PageURL,
+                                UpdatedBy = Convert.ToInt32(query[i].UpdatedBy),
+                                UpdatedAt = Convert.ToDateTime(query[i].UpdatedAt)
+                            });
+                        }
+                    }
+                }
+                
+
+            }
+            catch (Exception e)
+            {
+                res.HasError = true;
+                res.ErrorMessage = e.Message;
+            }
+
+
+            return res;
+        }
+
+        [HttpGet]
+        public BookListPLFT GetSortedListByLFT()
+        {
+            BookListPLFT res = new BookListPLFT();
+            try
+            {
+                using (KnilaTestEntities context = new KnilaTestEntities())
+                {
+                    var query = (from PU in context.tblBook
+                                 orderby PU.AuthorLastName, PU.AuthorFirstName, PU.Title
+                                 select PU).ToList();
+                    if (query.Count() > 0)
+                    {
+
+                        for (int i = 0; i < query.Count; i++)
+                        {
+                            res.bookList.Add(new BookDatatype
+                            {
+                                AuthorLastName = query[i].AuthorLastName,
+                                AuthorFirstName = query[i].AuthorFirstName,
+                                Publisher = query[i].Publisher,
+                                Title = query[i].Title,
+                                Price = Convert.ToDecimal(query[i].Price),
+                                TitleofContainer = query[i].TitleofContainer,
+                                PublicationDate = Convert.ToDateTime(query[i].PublicationDate),
+                                PageNo = Convert.ToInt32(query[i].PageNo),
+                                JournalTitle = query[i].JournalTitle,
+                                Issueno = query[i].Issueno,
+                                VolumeNo = query[i].VolumeNo,
+                                PageRange = query[i].PageRange,
+                                PageURL = query[i].PageURL,
+                                UpdatedBy = Convert.ToInt32(query[i].UpdatedBy),
+                                UpdatedAt = Convert.ToDateTime(query[i].UpdatedAt)
+                            });
+                        }
+                    }
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                res.HasError = true;
+                res.ErrorMessage = e.Message;
+            }
+
+
+            return res;
+        }
+    }
+}
---
+++ BookApp/Models/BookDatatype.cs
@@ -0,0 +1,56 @@
+﻿using System.Net.NetworkInformation;
+using System.Runtime.Serialization;
+using System.Security.Principal;
+
+namespace BookApp.Models
+{
+    public class BookDatatype
+    {
+        public int AuthorID { get; set; }
+        public string Publisher { get; set; }
+        public string Title { get; set; }
+        public string TitleofSource { get; set; }
+        public string AuthorLastName { get; set; }
+        public string AuthorFirstName { get; set; }
+        public decimal Price { get; set; }
+        public string TitleofContainer { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public int PageNo { get; set; }
+        public string JournalTitle { get; set; }
+        public string Issueno { get; set; }
+        public string VolumeNo { get; set; }
+        public string PageRange { get; set; }
+        public string PageURL { get; set; }
+        public bool isActive { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+    [DataContract]
+    public class BookListRequest
+    {
+        [DataMember]
+        public List<BookDatatype> BookList { get; set; }
+    }
+    [DataContract]
+    public class BookListResult
+    {
+        [DataMember]
+        public string ErrorMessage { get; set; }
+        [DataMember]
+        public bool HasError { get; set; }
+    }
+    [DataContract]
+    public class BookListPLFT : BookListResult
+    {
+        [DataMember]        public List<BookDatatype> bookList { get; set; }
+
+    }
+    [DataContract]
+    public class BookPrice : BookListResult
+    {
+        [DataMember] public decimal SumofPrice { get; set; }
+
+    }
+}
